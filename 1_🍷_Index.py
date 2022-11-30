b873899900c9767af79fa57353cc2277719737a5
chieh1002 <93277377+chieh1002@users.noreply.github.com>
2022-11-30 23:32:20 +0800

    Update 1_🍷_Index.py

--- 1_🍷_Index.py
+++ 1_🍷_Index.py
@@ -10,3 +10,17 @@ st.set_page_config(
 st.sidebar.success('Select a page.')
 
 st.title('Wine Roadmap')
+st.header(“Life is struggle, you deserve a wine”)
+
+st.markdown("""
+This website is designed for both **wine beginners** and **wine lovers**.
+
+If you are a person who has no idea which wine to be your first choice, go to 🍇_Flavor page.
+
+If you are a person who has already had your own wine testing preference list, go to 👍_Recommendation page.
+
+After you click **make recommendation**. You can go to 💭_Wordcloud page to take a look at your result.
+
+Also, we provide 🗺️_Visualization page to visualize PCA, t-SNE, and MDS.
+
+""")
