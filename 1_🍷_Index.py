b54aee0ecc0327a0213b8d936b0c6890447eae25
chieh1002 <93277377+chieh1002@users.noreply.github.com>
2022-12-01 19:53:05 +0800

    Update 1_🍷_Index.py

--- 1_🍷_Index.py
+++ 1_🍷_Index.py
@@ -12,10 +12,9 @@ st.sidebar.success('Select a page.')
 st.title('Wine Roadmap')
 
 st.markdown("""
-## This website is designed for both **wine beginners** and **wine lovers**.
+### This website is designed for both **wine beginners** and **wine lovers**.
 
-### If you are a person who has no idea which wine to be your first choice
-### --> click 🍇_Flavor page
+#### If you are a person who has no idea which wine to be your first choice --> click 🍇_Flavor page
 
 In the 🍇_Flavor page, you can find 13 different wine flavors. Try to rank your flavor preference. We will recommend suitable wine based on your choice.
 
@@ -26,8 +25,7 @@ Tips: you can either
 2.	search words from the word cloud as the keyword on the Internet OR
 3.	click 🗺️_Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer the closer cluster as well since the flavor might be similar and perhaps could be one of your choices.
 
-### If you are a person who has already had your own wine testing preference list
-### --> click 👍_Recommendation page
+#### If you are a person who has already had your own wine testing preference list --> click 👍_Recommendation page
 
 In the 👍_Recommendation page, there is a dataframe which contains all wines on the Vivino website. You can search and find your favorite wine. If you check a wine, you will find the wine you pick will show on the **Your Wine List**.
 
@@ -40,6 +38,6 @@ Tips: you can
 1.	Save wordcloud images of both Favorite List and Recommend List. Compare two wordcloud to see the similarity and difference. OR
 2.	click 🗺️_Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer the closer cluster as well since the flavor might be similar and perhaps could be one of your choices.
 
-### Hope you enjoy this website. Life is struggle, you deserve a wine
+#### Hope you enjoy this website. Life is struggle, you deserve a wine
 """)
 
