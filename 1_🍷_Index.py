7fa6f738f865d28693eb07a90e12b55edfd7ab93
sheisnt4046 <38826616+sheisnt4046@users.noreply.github.com>
2022-12-02 22:26:55 +0800

    modify text

--- 1_🍷_Index.py
+++ 1_🍷_Index.py
@@ -12,34 +12,34 @@ st.sidebar.success('Select a page.')
 st.title('Wine Roadmap')
 
 st.markdown("""
-### This website is designed for both **wine beginners** and **wine lovers**.
+### This application is designed for both **wine beginners** and **wine lovers**.
 
-#### If you are a person who has no idea which wine to be your first choice --> click 🍇_Flavor page
+#### If you have no idea which wine to be your first choice --> click 🍇Flavor page
 
-In the 🍇_Flavor page, you can find 13 different wine flavors. Try to rank your flavor preference. We will recommend suitable wine based on your choice.
+On the 🍇Flavor page, you can find 13 different wine flavors. Try to rank your flavor preference. We will recommend suitable wine based on your choice.
 
-Once you click `make recommendation` and get the wine list, you can go to 💭_Wordcloud page. In the 💭_Wordcloud page, choose `Flavor Formula`, and click `Make Wordcloud`. Then, we will generate word cloud for you based on your preference.
+Once you click `make recommendation` and get the wine list, you can go to 💭Wordcloud page. In the 💭Wordcloud page, choose `Flavor Formula`, and click `Make Wordcloud`. Then, we will generate Wordcloud for you based on your preference.
 
 Tips: you can either 
-1.	follow the recommendation list we provided for you *OR*
-2.	search words from the word cloud as the keyword on the Internet *OR*
-3.	click 🗺️_Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer the closer cluster as well since the flavor might be similar and perhaps could be one of your choices.
+1.	Follow the recommendation list we provided for you *OR*
+2.	Search words from the word cloud as the keyword on the Internet *OR*
+3.	Click 🗺️Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer to the closest cluster since the flavor might be similar and perhaps could be one of your choices.
 
-#### If you are a person who has already had your own wine testing preference list --> click 👍_Recommendation page
+#### If you already have your own wine testing preference list --> click 👍Recommendation page
 
-In the 👍_Recommendation page, there is a dataframe which contains all wines on the Vivino website. You can search and find your favorite wine. If you check a wine, you will find the wine you pick will show on the `Your Wine List`.
+On the 👍Recommendation page, there is a dataframe which contains all wines on the Vivino website. You can search and find your favorite wine. If you check a wine, you will find the wine you pick will show on the `Your Wine List`.
 
-Once you click `make recommendation` and get the wine list, you can go to 💭_Wordcloud page. In the 💭_Wordcloud page, you can choose either `Favorite List` or `Recommend List`. 
+Once you click `make recommendation` and get the wine list, you can go to 💭Wordcloud page. On the 💭Wordcloud page, you can choose either `Favorite List` or `Recommend List`. 
 
-If you choose `Favorite List` and click `Make Wordcloud`. Then, we will generate word cloud for you based on wines on your favorite list, which is `Your Wine List` on the 👍_Recommendation page.
+Suppose you choose `Favorite List` and click `Make Wordcloud`. Then, we will generate Wordcloud for you based on wines on your favorite list, which is `Your Wine List` on the 👍Recommendation page.
 
-If you choose `Recommend List` and click `Make Wordcloud`. Then, we will generate word cloud for you based on wines we recommend for you, which is `Your Recommend List` on the 👍_Recommendation page.
+Suppose you choose `Recommend List` and click `Make Wordcloud`. Then, we will generate Wordcloud for you based on wines we recommend for you, which is `Your Recommend List` on the 👍Recommendation page.
 
 
 Tips: you can either
-1.	Save wordcloud images of both Favorite List and Recommend List. Compare two wordcloud to see the similarity and difference. *OR*
-2.	click 🗺️_Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer the closer cluster as well since the flavor might be similar and perhaps could be one of your choices.
+1.	Save Wordcloud images of both Favorite List and Recommend List. Compare two Wordcloud to see the similarity and differences. *OR*
+2.	Click 🗺️Visualization page and find the cluster, which keywords also show on your word cloud. Also, you can refer to the closest cluster since the flavor might be similar and perhaps could be one of your choices.
 
-#### Hope you enjoy this website. Life is struggle, you deserve a wine!
+#### Hope you enjoy this website. Life is a struggle, you deserve wine!
 """)
 
--- pages/🗺️_Visualization.py
+++ pages/🗺️_Visualization.py
@@ -7,12 +7,17 @@ st.title('Visualization')
 def read_chart(title="t-SNE"):
     return plotly.io.read_json("plotly/%s.json" % title)
 
+st.subheader("Visualization of the K-means clusters (k=32) by t-SNE transformation")
+st.caption('T-SNE well separates most clusters while losing some information about inter-cluster relationships.')
 fig = read_chart("t-SNE")
 st.plotly_chart(fig)
 
+st.subheader("Visualization of the K-means clusters (k=32) by MDS transformation")
+st.caption('MDS visualizes the clusters with preserved intra-cluster distances and shows some equivocal instances around the cluster margins.')
 fig = read_chart("MDS")
 st.plotly_chart(fig)
 
+st.subheader("Visualization of the K-means clusters (k=32) by PCA transformation")
+st.caption('PCA demonstrates two major branches of the wines with a contact zone in which there are multiple indistinguishable clusters.')
 fig = read_chart("PCA")
 st.plotly_chart(fig)
-
