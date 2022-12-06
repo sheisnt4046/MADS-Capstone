7f2397a8dbb22e89331fd22befcc9b9b1458cccf
sheisnt4046 <38826616+sheisnt4046@users.noreply.github.com>
2022-12-06 17:03:57 +0800

    change variables for cache

--- pages/🗺️_Visualization.py
+++ pages/🗺️_Visualization.py
@@ -4,20 +4,20 @@ import plotly
 st.title('Visualization')
 
 @st.cache
-def read_chart(title="t-SNE"):
+def read_chart(title):
     return plotly.io.read_json("plotly/%s.json" % title)
 
 st.subheader("Visualization of the K-means clusters (k=32) by t-SNE transformation")
 st.caption('T-Distributed Stochastic Neighbor Embedding (t-SNE) well separates most clusters while losing some information about inter-cluster relationships.')
-fig = read_chart("t-SNE")
-st.plotly_chart(fig)
+fig1 = read_chart("t-SNE")
+st.plotly_chart(fig1)
 
 st.subheader("Visualization of the K-means clusters (k=32) by MDS transformation")
 st.caption('Multidimensional Scaling (MDS) visualizes the clusters with preserved intra-cluster distances and shows some equivocal instances around the cluster margins.')
-fig = read_chart("MDS")
-st.plotly_chart(fig)
+fig2 = read_chart("MDS")
+st.plotly_chart(fig2)
 
 st.subheader("Visualization of the K-means clusters (k=32) by PCA transformation")
 st.caption('Principal Components Analysis (PCA) demonstrates two major branches of the wines with a contact zone in which there are multiple indistinguishable clusters.')
-fig = read_chart("PCA")
-st.plotly_chart(fig)
+fig3 = read_chart("PCA")
+st.plotly_chart(fig3)
